15582495d5299bf60a7b9d72c2221230345ab0f6
Robert Birek <robert.birek@op.pl>
2025-04-13 21:54:11 +0100

    v3.2

--- scraper_db.py
+++ scraper_db.py
@@ -26,7 +26,7 @@ class Database:
                 if slug.offer_id not in seen:
                     seen.add(slug.offer_id)
                     unique_slugs.append(slug)
-            return unique_slugs
+            return unique_slugs[:5]
         finally:
             session.close()
 
